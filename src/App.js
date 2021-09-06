c1f6ed09a8197c156f8bb56226523c9113455816
angelsf <drifterkin_93@hotmail.com>
2021-09-05 22:39:12 -0400

    1.2: course information, step2

--- src/App.js
+++ src/App.js
@@ -6,31 +6,21 @@ import Part from "./part1/part/Part";
 import Total from "./part1/total/Total";
 
 function App() {
-    const course = {
-        name: 'Half Stack application development',
-        parts: [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10
-            },
-            {
-                name: 'Using props to pass data',
-                exercises: 7
-            },
-            {
-                name: 'State of a component',
-                exercises: 14
-            }
-        ]
-    }
+    const course = 'Half Stack application development'
+    const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+    const part2 = 'Using props to pass data'
+    const exercises2 = 7
+    const part3 = 'State of a component'
+    const exercises3 = 14
 
     return (
         <div>
-            <Header course={course.name}/>
-            <Part part={course.parts[0].name} exercises={course.parts[0].exercises}/>
-            <Part part={course.parts[1].name} exercises={course.parts[1].exercises}/>
-            <Part part={course.parts[2].name} exercises={course.parts[2].exercises}/>
-            <Total total={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}/>
+            <Header course={course}/>
+            <Content part1={part1} exercises1={exercises1}
+                     part2={part2} exercises2={exercises2}
+                     part3={part3} exercises3={exercises3}/>
+            <Total total={exercises1 + exercises2 + exercises3}/>
         </div>
     )
 }
