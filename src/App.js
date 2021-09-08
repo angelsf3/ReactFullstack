adb3572af681e1a9831009890e2c464994a2fbce
angelsf <drifterkin_93@hotmail.com>
2021-09-08 19:20:19 -0400

    1.7: unicafe step2

--- src/App.js
+++ src/App.js
@@ -8,10 +8,19 @@ function App() {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
+    const data = { good, neutral, bad }
 
-    const handleGood = () => setGood(good + 1)
-    const handleNeutral = () => setNeutral(neutral + 1)
-    const handleBad = () => setBad(bad + 1)
+    const handleGood = () => {
+        setGood(good + 1)
+    }
+
+    const handleNeutral = () => {
+        setNeutral(neutral + 1)
+    }
+
+    const handleBad = () => {
+        setBad(bad + 1)
+    }
 
     return (
         <div>
@@ -19,7 +28,7 @@ function App() {
             <Button handleClick={handleGood} text="good" value={good}/>
             <Button handleClick={handleNeutral} text="neutral" value={neutral}/>
             <Button handleClick={handleBad} text="bad" value={bad}/>
-            <Statistics good={good} neutral={neutral} bad={bad}/>
+            <Statistics data={data}/>
         </div>
     )
 }
--- src/part1/unicafe/statistics/Statistics.js
+++ src/part1/unicafe/statistics/Statistics.js
@@ -1,12 +1,20 @@
 import React from "react";
 
-function Statistics({ good, neutral, bad }) {
+function Statistics({ data }) {
+    const { good, neutral, bad } = data
+    const all = good + neutral + bad
+    const average = (all > 0) ? (good / all) - (bad / all) : 0
+    const positive = (all > 0) ? (good / all) * 100 : 0
+
     return(
         <div>
             <h1>statistics</h1>
             <p>good {good}</p>
             <p>neutral {neutral}</p>
             <p>bad {bad}</p>
+            <p>all {all}</p>
+            <p>average {average}</p>
+            <p>positive {positive}%</p>
         </div>
     )
 }
