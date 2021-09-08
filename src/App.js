e54b8b24615fdbb8ad0af79cf3635ad8c54b2078
angelsf <drifterkin_93@hotmail.com>
2021-09-08 17:01:14 -0400

    1.6: unicafe step1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import './App.css';
-import React from "react";
-import Header from "./part1/header/Header";
-import Content from "./part1/content/Content";
-import Part from "./part1/part/Part";
-import Total from "./part1/total/Total";
+import React, {useState} from "react";
+import Button from "./part1/unicafe/button/Button";
+import Statistics from "./part1/unicafe/statistics/Statistics";
 
 function App() {
-    const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
+
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    const handleGood = () => setGood(good + 1)
+    const handleNeutral = () => setNeutral(neutral + 1)
+    const handleBad = () => setBad(bad + 1)
 
     return (
         <div>
-            <Header course={course}/>
-            <Content part1={part1} exercises1={exercises1}
-                     part2={part2} exercises2={exercises2}
-                     part3={part3} exercises3={exercises3}/>
-            <Total total={exercises1 + exercises2 + exercises3}/>
+            <h1>give feedback</h1>
+            <Button handleClick={handleGood} text="good" value={good}/>
+            <Button handleClick={handleNeutral} text="neutral" value={neutral}/>
+            <Button handleClick={handleBad} text="bad" value={bad}/>
+            <Statistics good={good} neutral={neutral} bad={bad}/>
         </div>
     )
 }
