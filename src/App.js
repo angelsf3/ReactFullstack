8911d5d4e743459bdf388b6798fe88001a1a8907
angelsf <drifterkin_93@hotmail.com>
2021-09-08 22:19:51 -0400

    1.12*: anecdotes step1

--- src/App.js
+++ src/App.js
@@ -1,34 +1,27 @@
 import './App.css';
 import React, {useState} from "react";
 import Button from "./part1/unicafe/button/Button";
-import Statistics from "./part1/unicafe/statistics/Statistics";
 
 function App() {
+    const anecdotes = [
+        'If it hurts, do it more often',
+        'Adding manpower to a late software project makes it later!',
+        'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+        'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+        'Premature optimization is the root of all evil.',
+        'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+        'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+    ]
 
-    const [good, setGood] = useState(0)
-    const [neutral, setNeutral] = useState(0)
-    const [bad, setBad] = useState(0)
-    const data = { good, neutral, bad }
+    const [selected, setSelected] = useState(0)
 
-    const handleGood = () => {
-        setGood(good + 1)
-    }
-
-    const handleNeutral = () => {
-        setNeutral(neutral + 1)
-    }
-
-    const handleBad = () => {
-        setBad(bad + 1)
-    }
+    const nextNote = () => setSelected(Math.floor(Math.random() * anecdotes.length))
 
     return (
         <div>
-            <h1>give feedback</h1>
-            <Button handleClick={handleGood} text="good" value={good}/>
-            <Button handleClick={handleNeutral} text="neutral" value={neutral}/>
-            <Button handleClick={handleBad} text="bad" value={bad}/>
-            <Statistics data={data}/>
+            {anecdotes[selected]}
+            <br/>
+            <Button text="next anecdote" handleClick={nextNote}/>
         </div>
     )
 }
