00c60368b6214ff85c833ff54c9ababcd9ebdc46
angelsf <drifterkin_93@hotmail.com>
2021-09-08 20:06:28 -0400

    1.10: unicafe step5

--- src/part1/unicafe/statistics/Statistics.js
+++ src/part1/unicafe/statistics/Statistics.js
@@ -1,4 +1,5 @@
 import React from "react";
+import StatisticLine from "../statistic-line/StatisticLine";
 
 function Statistics({ data }) {
     const { good, neutral, bad } = data
@@ -16,12 +17,12 @@ function Statistics({ data }) {
     return(
         <div>
             <h1>statistics</h1>
-            <p>good {good}</p>
-            <p>neutral {neutral}</p>
-            <p>bad {bad}</p>
-            <p>all {all}</p>
-            <p>average {average}</p>
-            <p>positive {positive}%</p>
+            <StatisticLine text="good" value={good}/>
+            <StatisticLine text="neutral" value={neutral}/>
+            <StatisticLine text="bad" value={bad}/>
+            <StatisticLine text="all" value={all}/>
+            <StatisticLine text="average" value={average}/>
+            <StatisticLine text="positive" value={positive}/>
         </div>
     )
 }
