5a622983d900cbc1fa4f9d514ad79e50e14d8ed3
angelsf <drifterkin_93@hotmail.com>
2021-09-13 22:43:41 -0400

    2.3*: Course information step8

--- src/course/Course.js
+++ src/course/Course.js
@@ -4,13 +4,14 @@ import Part from "./part/Part";
 import Total from "./total/Total";
 
 function Course({ course }) {
+    let total = course.parts.map(p => p.exercises).reduce((a, b) => a + b)
     return (
         <div>
             <Header course={course.name}/>
             {course.parts.map(part =>
                 <Part key={part.id} part={part.name} exercises={part.exercises}/>
             )}
-            <Total total={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}/>
+            <Total total={total}/>
         </div>
     )
 }
